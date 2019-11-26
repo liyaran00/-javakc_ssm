196a56cacced1c5de9285211ad416ebb173157c3
lidabao <12345@qq.com>
2019-11-26 21:00:01 +0800

    提交MSRelation模块

--- src/main/java/com/zhg/javakc/modules/mdata/msrelation/controller/MSController.java
+++ src/main/java/com/zhg/javakc/modules/mdata/msrelation/controller/MSController.java
@@ -1,10 +1,15 @@
 package com.zhg.javakc.modules.mdata.msrelation.controller;
 
+import com.zhg.javakc.base.page.Page;
+import com.zhg.javakc.modules.mdata.msrelation.entity.MSEntity;
 import com.zhg.javakc.modules.mdata.msrelation.service.MSService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Lee
@@ -19,8 +24,14 @@ public class MSController {
     private MSService msService;
 
     @RequestMapping("queryMS")
-    public ModelAndView queryMS(){
+    public ModelAndView queryMS(MSEntity entity, HttpServletRequest request, HttpServletResponse response){
+        //modelAndView对应的view是jsp：/view/test/list.jsp
         ModelAndView modelAndView = new ModelAndView("msrelation/list");
+        //分页查询
+        Page<MSEntity> page =msService.queryMS(entity, new Page<MSEntity>(request, response));
+        //modelAndView对应的model就是page
+        modelAndView.addObject("page",page);
+        //携带数据返回页面
         return modelAndView;
     }
 
